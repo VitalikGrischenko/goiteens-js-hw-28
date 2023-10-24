256c3f8bfb3984cd37aada086e534ef665324108
VitalikGrischenko <110487381+VitalikGrischenko@users.noreply.github.com>
2023-10-24 16:46:18 +0300

    dfjedzujeuy

--- js/1.js
+++ js/1.js
@@ -1,19 +1,35 @@
-const imagesRef = document.querySelectorAll('.image');
-const galleryRef = document.querySelector('.gallery');
+const galleryRef = document.querySelector("[ data-gallery]");
+const galleryLiRef = galleryRef.children;
+const galleryImgRef = document.querySelector("[data-galleryimg=image]");
+const galleryDivRef = document.querySelector("[data-gallerydiv]");
 
+let i = 0;
 
-window.addEventListener('keydown', (e) => {
-  if (e.key === 'ArrowLeft') {
-    showImage(currentImageIndex - 1);
-  } else if (e.key === 'ArrowRight') {
-    showImage(currentImageIndex + 1);
-  }
-});
+galleryImgRef.addEventListener("click", (e) => {
+  galleryDivRef.classList.toggle("show-gallery-modal");
+  document.addEventListener("keydown", (event) => {
+    if (event.code === "ArrowRight" && i < galleryLiRef.length - 1) {
+      galleryLiRef[i].classList.remove("full-image-container");
+      galleryLiRef[i].firstElementChild.classList.remove("full-image");
 
-images.forEach((imagesRef, index) => {
-  imagesRef.addEventListener('click', () => {
-    showImage(index);
+      galleryLiRef[i + 1].classList.add("full-image-container");
+      galleryLiRef[i + 1].firstElementChild.classList.add("full-image");
+      i++;
+    }
+
+    if (event.code === "ArrowLeft" && i < galleryLiRef.length - 1) {
+      galleryLiRef[i].classList.remove("full-image-container");
+      galleryLiRef[i].firstElementChild.classList.remove("full-image");
+
+      galleryLiRef[i - 1].classList.add("full-image-container");
+      galleryLiRef[i - 1].firstElementChild.classList.add("full-image");
+      i--;
+    }
   });
 });
 
-showImage(currentImageIndex);
+galleryDivRef.addEventListener("click", (e) => {
+  if (e.target.classList.contains("full-image-container")) {
+    galleryDivRef.classList.toggle("show-gallery-modal");
+  }
+});
